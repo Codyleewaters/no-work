5082db35801419e3a23d9f3f4a8570c1b6b83d30
Codyleewaters <92151225+Codyleewaters@users.noreply.github.com>
2021-10-08 09:31:11 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,15 @@
 input.onButtonPressed(Button.A, function () {
 	
 })
+basic.showLeds(`
+    . . # . .
+    . . # . .
+    . . # . .
+    . # # # .
+    . . # . .
+    `)
+basic.showIcon(IconNames.Heart)
+basic.showString("cody")
 basic.forever(function () {
 	
 })
